7f8025af55de65fa8c59d5411f504a2cff9fd43c
balasahebgulave <balasaheb.gulave94@gmail.com>
2019-01-16 12:07:43 +0530

    final commit

--- user_login/forms.py
+++ user_login/forms.py
@@ -3,9 +3,6 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class UserForm(UserCreationForm):
-    username= forms.CharField()
-    email = forms.EmailField()
-    
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
--- user_login/views.py
+++ user_login/views.py
@@ -1,53 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from user_login.forms import UserForm
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate
 
 
-
-
-
 # custom View for SignUp
-def register(request):
-    if request.method == 'POST':
-        form = UserForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            raw_password = form.cleaned_data['password2']
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            form.save()
-            return HttpResponseRedirect('register')
-
-    else: 
-        form = UserForm()
-    return render (request , 'register.html' , {'form':form})
-
-
-
-# django model for SignUp
-
-# from django.contrib.auth import login, authenticate
-# from django.contrib.auth.forms import UserCreationForm
-# from django.shortcuts import render, redirect
-
-# def register(request):
-#     if request.method == 'POST':
-#         form = UserCreationForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             username = form.cleaned_data.get('username')
-#             raw_password = form.cleaned_data.get('password1')
-#             user = authenticate(username=username, password=raw_password)
-#             login(request, user)
-#             return redirect('register')
-#     else:
-#         form = UserCreationForm()
-#     return render(request, 'register.html', {'form': form})
-
-# Create your views here.
 
 
 def home(request):
     return render (request , 'base.html')
+
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('login')
+    else:
+        form = UserForm()
+    return render(request, 'register.html', {'form': form})
